fdd0c41753a305dc295a0436c4cc93854907885d
MUHAMMAD YOUSAF RANA <21-se-58@students.uettaxila.edu.pk>
2025-04-18 12:47:46 +0000

    main updated

--- main.py
+++ main.py
@@ -1,36 +1,24 @@
-from fastapi import FastAPI, HTTPException
+
+from transformers import pipeline
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
-from my_lib.summarize import load_model_and_tokenizer, summarize_text
-from typing import Tuple
-import torch
+
+generator = pipeline('text-generation', model='gpt2')
 
 app = FastAPI()
 
-# Global variables for model and tokenizer
-tokenizer = None
-model = None
 
-class TextRequest(BaseModel):
+class Body(BaseModel):
     text: str
 
-@app.on_event("startup")
-async def startup_event():
-    global tokenizer, model
-    tokenizer, model = load_model_and_tokenizer()
-    print("Model and tokenizer loaded successfully.")
 
-@app.get("/")
-async def root():
-    return {"message": "Welcome to the Summarization API. Use /summarize to summarize text."}
+@app.get('/')
+def root():
+    return HTMLResponse("<h1>A self-documenting API to interact with a GPT2 model and generate text</h1>")
 
-@app.post("/summarize")
-async def summarize_api(request: TextRequest):
-    if not request.text.strip():
-        raise HTTPException(status_code=400, detail="Text cannot be empty")
 
-    try:
-        # Summarize large text by processing paragraph by paragraph
-        summary = summarize_text(request.text, tokenizer, model)
-        return {"summary": summary}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error during summarization: {str(e)}")
+@app.post('/generate')
+def predict(body: Body):
+    results = generator(body.text, max_length=35, num_return_sequences=1)
+    return results[0]
